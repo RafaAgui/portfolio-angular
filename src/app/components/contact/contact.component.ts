e980b151774fc044eb27ece54ea837a66dced5b1
RafaAgui <aguilaralvear@yahoo.es>
2023-12-05 14:04:31 +0100

    fix: update repo

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
 
+  ngOnInit(){
+    $('.gallery img').on('click', function() {
+      var imgUrl = $(this).attr('src');
+      var imgAlt = $(this).attr('alt');
+
+      var lightbox = '<div class="lightbox">' +
+          '<img src="' + imgUrl + '" alt="' + imgAlt + '">' +
+          '</div>';
+
+      $('main').append(lightbox);
+
+      $('.lightbox').on('click', function() {
+          $(this).remove();
+      });
+  });
+  }
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef  } from '@angular/core';
 import { Project } from '../../models/project'
 import { ProjectsService } from '../../services/project.service'
 
@@ -8,13 +8,31 @@ import { ProjectsService } from '../../services/project.service'
   styleUrls: ['./projects.component.scss'],
   providers: [ProjectsService]
 })
-export class ProjectsComponent implements OnInit  {
+export class ProjectsComponent implements OnInit, AfterViewInit  {
   public projects: Project[];
   public title: string;
 
-  constructor(private _projectService: ProjectsService){
+  constructor(
+    private _projectService: ProjectsService,
+    private el: ElementRef){
     this.title = 'Proyectos realizados';
     this.projects = [];
+  }
+
+  ngAfterViewInit() {
+    // Este método se llama después de que la vista del componente se ha inicializado
+    this.agregarClaseCuandoCargado();
+  }
+
+  private agregarClaseCuandoCargado() {
+    // Lógica para determinar si la página se ha cargado completamente
+    // Aquí puedes usar eventos del DOM o funciones de terceros según tus necesidades
+
+    // Por ejemplo, puedes usar el evento load del objeto window
+    window.addEventListener('load', () => {
+      // La página se ha cargado completamente, ahora agrega la clase al host
+      this.el.nativeElement.classList.add('loaded');
+    });
   }
 
   ngOnInit(){
